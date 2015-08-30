b25e009cd1d3f113c5c5e46d932166cb7e5e618f
fdanesse <fdanesse@gmail.com>
2015-08-30 11:19:47 -0300

    Correcciones

--- v4/gtkWidgets/Globales.py
+++ v4/gtkWidgets/Globales.py
@@ -5,9 +5,20 @@
 #   Flavio Danesse <fdanesse@gmail.com>
 #   Uruguay
 
-import os
-import json
-import codecs
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import socket
 
 
@@ -20,31 +31,3 @@ def get_ip():
     except:
         pass
     return ip
-
-'''
-MAKELOG = True
-LOGPATH = os.path.join(os.environ["HOME"], "JAMTank_load.log")
-
-
-def reset_log():
-    if os.path.exists(LOGPATH):
-        os.remove(LOGPATH)
-
-
-def WRITE_LOG(_dict):
-    archivo = open(LOGPATH, "w")
-    archivo.write(json.dumps(
-        _dict, indent=4, separators=(", ", ":"), sort_keys=True))
-    archivo.close()
-
-
-def APPEND_LOG(_dict):
-    new = {}
-    if os.path.exists(LOGPATH):
-        archivo = codecs.open(LOGPATH, "r", "utf-8")
-        new = json.JSONDecoder("utf-8").decode(archivo.read())
-        archivo.close()
-    for key in _dict.keys():
-        new[key] = _dict[key]
-    WRITE_LOG(new)
-'''
