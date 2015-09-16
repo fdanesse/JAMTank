3f406d7ec162947d9ff4417afb0e0a26f4f3ae93
fdanesse <fdanesse@gmail.com>
2015-09-16 11:48:53 -0300

    StatusGame

--- v4/gtkWidgets/SingleStatusGame.py
+++ v4/gtkWidgets/SingleStatusGame.py
@@ -36,10 +36,9 @@ class SingleDialogoEndGame(gtk.Dialog):
         self.set_decorated(False)
         self.set_border_width(15)
 
-        self._ranking = Ranking()
+        self._ranking = Ranking(_dict.get(0, {}))
         self.vbox.pack_start(self._ranking, True, True, 5)
 
-        #self._ranking._lista.update(_dict)
         self.vbox.show_all()
         self.set_sensitive(False)
         gobject.timeout_add(4000, self.__sensitive)
@@ -62,140 +61,86 @@ class SingleStatusGame(gtk.Window):
         self.set_deletable(False)
         self.set_transient_for(top)
 
-        #self._ranking = Ranking()
-        #self._framejugador = FrameJugador(vidas)
+        self._framejugador = FrameJugador()
 
         vbox = gtk.VBox()
-        #vbox.pack_start(self._ranking, False, False, 0)
-        #vbox.pack_end(self._framejugador, False, False, 0)
+        vbox.pack_end(self._framejugador, False, False, 0)
         self.add(vbox)
         self.show_all()
 
     def update(self, juego, _dict):
-        #self._ranking._lista.update(_dict)
-        #self._framejugador.update(_dict.get(self._ip))
-        pass
+        new = _dict.get(0, {})
+        new["tanque"] = "t5.png"
+        self._framejugador.update(new)
 
 
 class Ranking(gtk.Frame):
 
-    def __init__(self):
+    def __init__(self, _dict):
 
         gtk.Frame.__init__(self)
 
         self.set_border_width(4)
-        self.set_label(" Ranking ")
         event = gtk.EventBox()
         event.set_border_width(4)
         event.set_property("visible-window", False)
-        self._lista = Lista()
-        event.add(self._lista)
+        vbox = gtk.VBox()
+
+        hbox = gtk.HBox()
+        self._puntos = gtk.Label("Puntos = %s" % 0)
+        hbox.pack_start(self._puntos, False, False, 0)
+        vbox.pack_start(hbox, False, False, 0)
+
+        hbox = gtk.HBox()
+        self._disparos = gtk.Label("Disparos = %s" % 0)
+        hbox.pack_start(self._disparos, False, False, 0)
+        vbox.pack_start(hbox, False, False, 0)
+
+        hbox = gtk.HBox()
+        self._aciertos = gtk.Label("Aciertos = %s" % 0)
+        hbox.pack_start(self._aciertos, False, False, 0)
+        vbox.pack_start(hbox, False, False, 0)
+
+        hbox = gtk.HBox()
+        self._porcenta = gtk.Label("Efectividad = %s" % 0)
+        hbox.pack_start(self._porcenta, False, False, 0)
+        vbox.pack_start(hbox, False, False, 0)
+
+        event.add(vbox)
         self.add(event)
         self.show_all()
-
-
-class Lista(gtk.TreeView):
-
-    def __init__(self):
-
-        gtk.TreeView.__init__(self, gtk.ListStore(gobject.TYPE_STRING,
-            gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING,
-            gobject.TYPE_INT))
-
-        self.set_property("rules-hint", True)
-        self.set_headers_clickable(False)
-        self.set_headers_visible(False)
-        self.__setear_columnas()
-        self.get_model().set_sort_column_id(4, gtk.SORT_DESCENDING)
-        self.show_all()
-
-    def __setear_columnas(self):
-        self.append_column(self.__construir_columa("Ip", 0, False))
-        self.append_column(self.__construir_columa_icono("Tanque", 1, True))
-        self.append_column(self.__construir_columa("Path", 2, False))
-        self.append_column(self.__construir_columa("Nombre", 3, True))
-        self.append_column(self.__construir_columa("Puntos", 4, True))
-
-    def __construir_columa(self, text, index, visible):
-        render = gtk.CellRendererText()
-        columna = gtk.TreeViewColumn(text, render, text=index)
-        columna.set_sort_column_id(index)
-        columna.set_property("visible", visible)
-        columna.set_property("resizable", False)
-        columna.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
-        return columna
-
-    def __construir_columa_icono(self, text, index, visible):
-        render = gtk.CellRendererPixbuf()
-        columna = gtk.TreeViewColumn(text, render, pixbuf=index)
-        columna.set_property("visible", visible)
-        columna.set_property("resizable", False)
-        columna.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
-        return columna
-
-    def __ejecutar_agregar_elemento(self, elementos):
-        if not elementos:
-            return False
-        ip, pixbuf, path, nick, puntos = elementos[0]
-        self.get_model().append([ip, pixbuf, path, nick, puntos])
-        elementos.remove(elementos[0])
-        gobject.idle_add(self.__ejecutar_agregar_elemento, elementos)
-        return False
-
-    def __buscar(self, texto):
-        model = self.get_model()
-        _iter = model.get_iter_first()
-        while _iter:
-            contenido = model.get_value(_iter, 0)
-            if texto == contenido:
-                return _iter
-            _iter = model.iter_next(_iter)
-        return None
-
-    def agregar_items(self, elementos):
-        gobject.idle_add(self.__ejecutar_agregar_elemento, elementos)
+        if _dict:
+            self.update(_dict)
 
     def update(self, _dict):
-        try:
-            items = []
-            for ip in _dict.keys():
-                _iter = self.__buscar(ip)
-                if _iter:
-                    model = self.get_model()
-                    model.set_value(_iter, 4, _dict[ip]["s"]["p"])
-                else:
-                    nick = _dict[ip]["n"]
-                    puntos = _dict[ip]["s"]["p"]
-                    path = os.path.join(BASE, "Tanques", _dict[ip]["t"])
-                    pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(path, 50, -1)
-                    items.append([ip, pixbuf, path, nick, puntos])
-            self.agregar_items(items)
-        except:
-            print "ERROR:", self.update, _dict
+        self._puntos.set_text("Puntos = %s" % _dict.get("puntos", 0))
+        self._disparos.set_text("Disparos = %s" % _dict.get("disparos", 0))
+        self._aciertos.set_text("Aciertos = %s" % _dict.get("aciertos", 0))
+        self._porcenta.set_text("Efectividad = %s" % (
+            _dict.get("aciertos", 0) * 100 / _dict.get("disparos", 0)))
 
 
 class FrameJugador(gtk.Frame):
 
-    def __init__(self, vidas):
+    def __init__(self):
 
         gtk.Frame.__init__(self)
 
-        self._max_vidas = vidas
         self.set_border_width(4)
-        self.set_label(" Jugador ")
 
         event = gtk.EventBox()
         event.set_border_width(4)
         event.set_property("visible-window", False)
 
         self._preview = gtk.Image()
+        self._pixbuf = False
+        self._ranking = Ranking({})
         self._energia = FrameProgress("Energía:")
-        self._vidas = FrameProgress("Vidas:")
 
         vbox = gtk.VBox()
+        vbox.pack_start(self._ranking, False, False, 0)
         vbox.pack_start(self._preview, False, False, 0)
         vbox.pack_start(self._energia, False, False, 0)
-        vbox.pack_start(self._vidas, False, False, 0)
 
         event.add(vbox)
         self.add(event)
@@ -203,14 +148,12 @@ class FrameJugador(gtk.Frame):
 
     def update(self, _dict):
         try:
-            nick = _dict["n"]
-            if self.get_label() != nick:
-                self.set_label(" %s " % nick)
-                path = os.path.join(BASE, "Tanques", _dict["t"])
-                pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(path, 80, -1)
-                self._preview.set_from_pixbuf(pixbuf)
-            self._energia.update(100, _dict["s"]["e"])
-            self._vidas.update(self._max_vidas, _dict["s"]["v"])
+            if not self._pixbuf:
+                path = os.path.join(BASE, "Tanques", _dict["tanque"])
+                self._pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(path, 80, -1)
+                self._preview.set_from_pixbuf(self._pixbuf)
+            self._ranking.update(_dict)
+            self._energia.update(100, _dict["energia"])
         except:
             print "ERROR:", self.update, _dict
 
@@ -278,10 +221,6 @@ class ProgressBar(gtk.HScale):
         x, y, w, h = self.get_allocation()
         gc = gtk.gdk.Drawable.new_gc(self.window)
 
-        # todo el widget
-        #gc.set_rgb_fg_color(gtk.gdk.Color(255, 255, 255))
-        #self.window.draw_rectangle(gc, True, x, y, w, h)
-
         # vacio
         gc.set_rgb_fg_color(gtk.gdk.Color(0, 0, 0))
         ww = w - 10 * 2
@@ -294,8 +233,4 @@ class ProgressBar(gtk.HScale):
         ximage = int(self.ajuste.get_value() * ww / 100)
         gc.set_rgb_fg_color(gtk.gdk.Color(23000, 41000, 12000))
         self.window.draw_rectangle(gc, True, xx, yy, ximage, hh)
-
-        # borde de progreso
-        #gc.set_rgb_fg_color(get_colors("window"))
-        #self.window.draw_rectangle(gc, False, xx, yy, ww, hh)
         return True
